7a9737f5946d1e6217d133b0c1fd5626abf96e5e
Jiacong Xu <jiacong.xu@foxmail.com>
2017-06-05 18:35:43 -0700

    Added game state.

--- Assets/Scripts/Game/Models/GameModel.cs
+++ Assets/Scripts/Game/Models/GameModel.cs
@@ -2,28 +2,36 @@
 using System;
 
 namespace Leap {
-  /// <summary>
-  /// Model for a game. Each level will be incrementally loaded into the game,
-  /// but they should be the geometry and not affect the model.
-  /// </summary>
-  public class GameModel {
-    int leapCount;
-
     /// <summary>
-    /// The number of leaps remaining for the player.
+    /// Model for a game. Each level will be incrementally loaded into the game,
+    /// but they should be the geometry and not affect the model.
     /// </summary>
-    public int LeapCount {
-      get {
-        return leapCount;
-      }
+    public class GameModel {
+        int leapCount;
 
-      set {
-        if (value < 0) {
-          Debug.LogError("Leap count cannot be negative!");
-        } else {
-          leapCount = value;
+        /// <summary>
+        /// The number of leaps remaining for the player.
+        /// </summary>
+        public int LeapCount {
+            get {
+                return leapCount;
+            }
+
+            set {
+                if (value < 0) {
+                    Debug.LogError("Leap count cannot be negative!");
+                } else {
+                    leapCount = value;
+                }
+            }
         }
-      }
+
+        /// <summary>
+        /// Stores the state of the game.
+        /// </summary>
+        public GameState GameState {
+            get;
+            set;
+        }
     }
-  }
 }
---
+++ Assets/Scripts/Game/Models/GameState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Leap {
+    /// <summary>
+    /// Describes various states during game play.
+    /// </summary>
+    public enum GameState {
+        Playing,
+        Paused,
+        Over
+    }
+}
